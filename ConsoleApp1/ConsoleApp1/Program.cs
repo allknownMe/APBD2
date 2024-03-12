d8fd4949faa171e6034b7226b71e91adfaf9057d
Oleh Serhiienko <allknown@Olehs-MacBook-Air.local>
2024-03-12 18:24:57 +0100

    Added ConsoleApp1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, User!");
+int[] arr = {1,2,3,4,5};
+Console.WriteLine(GetAvarage(arr));
+
+static double GetAvarage(int[] arr)
+{
+    int sum = 0;
+    foreach (var num in arr)
+    {
+        sum += num;
+    }
+    return (double)sum / arr.Length;
+}
